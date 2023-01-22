fdb654bc947d67d59f32e0b43f9881fe8042b5d7
Emil-ant <emiliosantoun411@gmail.com>
2023-01-22 01:41:43 +0000

    Added information about ASCII in ruby file

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,8 @@
+# NOTE: A-B in ASCII is 65-90 in decimal
+# NOTE: a-b in ASCII is 97-122 in decimal
+
+
+
 def caesar_cipher(string, shift)
     # convert string to array of characters
     characters = string.chars
@@ -5,6 +10,7 @@ def caesar_cipher(string, shift)
     shifted_chars = characters.map do |char|
         if char == " "
             char
+        # check if shift passes final letter in ASCII table 
         elsif char.ord.between?(65, 90)
             shifted_ascii = (char.ord - 65 + shift) % 26 + 65
             shifted_ascii.chr
