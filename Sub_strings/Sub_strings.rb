14a47507108bccd92d07b546b590af7a6b5f65f2
Emil-Ant <emiliosantoun411@gmail.com>
2023-01-23 15:54:19 +0000

    first lines]

--- Sub_strings/Sub_strings.rb
+++ Sub_strings/Sub_strings.rb
@@ -1,3 +1,15 @@
 # implement a method #substrings that takes a word as the first assignement and then an array of valid substrings (your dictionary)
 # as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and
 # how many times it was found
+
+# FIRST TODO: figure out how to find count for each element in the array
+
+Dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(word, array)
+
+
+
+
+
+end
