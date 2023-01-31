41afefc383969169d4beb55adf303b45ab0e917e
Emil-ant <emiliosantoun411@gmail.com>
2023-01-31 10:42:39 +0000

    final code and note to self

--- Sub_strings/Sub_strings.rb
+++ Sub_strings/Sub_strings.rb
@@ -2,22 +2,30 @@
 # as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and
 # how many times it was found
 
-# have to use #match 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 
-def substrings(word, dictionary)
-    word_array = []
-    # iterate over every word in dictionary and checking if it's it #includes the argument word. If it is, push it into word_array
+def substrings(string, dictionary)
+    word_hash = {}
+    string = string.downcase
     dictionary.each do |dict_word|
-        if word.downcase.include?(dict_word.downcase)
-            word_array.push(dict_word)
-        end
+        #if the word is found in string argument, use .scan method to count the number of occurences of that word 
+        word_hash[dict_word] = string.scan(dict_word).size if string.include?(dict_word)
     end 
-    
+    word_hash
 
 end
 
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+
+#note to self: 
+# The string.scan(dict_word).size line of code is checking the number of times that the substring dict_word occurs in the string and returning the size or count of those occurrences.
+
+# The string.scan(dict_word) method scans the string for all occurrences of the substring dict_word and returns an array of all matches.
+
+# For example, if string = "hello world" and dict_word = "o", string.scan(dict_word) would return ["o", "o"].
+
+# The .size method is then called on the result of string.scan(dict_word) to determine the number of elements in the array, which is equivalent to the number of times the substring was found in the string. So, in this example string.scan(dict_word).size would return 2.
