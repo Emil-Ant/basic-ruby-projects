bb1a22aa2e3fce4af09a11988e40f8ccc45582a8
Emil-Ant <emiliosantoun411@gmail.com>
2023-01-31 15:24:59 +0000

    Added #stock_picker method file :beer:

---
+++ Stock_picker/stock_picker.rb
@@ -0,0 +1,11 @@
+# implement a method #stock_picker that takes an array of stock prices 
+# One (stock price) for each hypothetical day
+# It should return a pair of days representing the best day to buy
+# and the best day to sell. Days start at 0. 
+
+
+
+def stock_picker(stock_prices)
+
+
+end
