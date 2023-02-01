36b4581a32d2c9523605a00ad7dc2ee132a66919
Emil-Ant <emiliosantoun411@gmail.com>
2023-02-01 10:53:04 +0000

    added array and messing around

--- Stock_picker/stock_picker.rb
+++ Stock_picker/stock_picker.rb
@@ -1,11 +1,10 @@
-# implement a method #stock_picker that takes an array of stock prices 
+# implement a method #stock_picker that takes an array of stock prices
 # One (stock price) for each hypothetical day
 # It should return a pair of days representing the best day to buy
-# and the best day to sell. Days start at 0. 
+# and the best day to sell. Days start at 0.
 
-
+list = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(stock_prices)
-
 
 end
