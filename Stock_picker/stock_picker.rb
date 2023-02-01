cc9b016d1e3c7a06e3d787674ba9ff7a5d85e8a9
Emil-Ant <emiliosantoun411@gmail.com>
2023-02-01 10:19:34 +0000

    typo

--- Stock_picker/stock_picker.rb
+++ Stock_picker/stock_picker.rb
@@ -1,6 +1,6 @@
 # implement a method #stock_picker that takes an array of stock prices 
 # One (stock price) for each hypothetical day
-# It should return a pair of days representing the best day to buy
+# It should return a pair of days representing the best day to buyw
 # and the best day to sell. Days start at 0. 
 
 
