49449e84222469096876d8216d83c781d22b6f6b
Emil-ant <emiliosantoun411@gmail.com>
2023-02-11 17:15:28 +0000

    added some progress on the python version of stock picker

--- Stock_picker/stock_picker.py
+++ Stock_picker/stock_picker.py
@@ -6,9 +6,14 @@
 list = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(stock_prices):
-    # iterates through list but stop 2 values before the full length of list becase can't sell there
-    for value in stock_prices[0:len(list)-2]:
-        print(value)
+    # iterates through list but stop 1 value before the full length of list becase can't sell on the last day
+    profit = 0 
+    maximum_profit = 0
+    for value in stock_prices[0:len(stock_prices)-1]:
+        value_index = stock_prices.index(value)
+
 
 stock_picker(list)
+
+# Current progress: trying to figure out how to go through list and basically do value (current iterable) - stock_prices at all the indexes that come after the index of value till the end of the list
     
