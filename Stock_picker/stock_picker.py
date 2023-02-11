8cf8f9915bad5df2823a1bff332e450fc4e84f0e
Emil-ant <emiliosantoun411@gmail.com>
2023-02-11 01:53:26 +0000

    added python file for stock picker

---
+++ Stock_picker/stock_picker.py
@@ -0,0 +1,14 @@
+# implement a method #stock_picker that takes an array of stock prices
+# One (stock price) for each hypothetical day
+# It should return a pair of days representing the best day to buy
+# and the best day to sell. Days start at 0.
+
+list = [17,3,6,9,15,8,6,1,10]
+
+def stock_picker(stock_prices):
+    # iterates through list but stop 2 values before the full length of list becase can't sell there
+    for value in stock_prices[0:len(list)-2]:
+        print(value)
+
+stock_picker(list)
+    
