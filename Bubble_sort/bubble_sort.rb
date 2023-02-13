ae8015b906357aade9c8b09d5a45ae754b485d8c
Emil-ant <emiliosantoun411@gmail.com>
2023-02-13 10:52:04 +0000

    added bubble sort final with example

--- Bubble_sort/bubble_sort.rb
+++ Bubble_sort/bubble_sort.rb
@@ -1,13 +1,17 @@
 # Build a method #bubble_sort that takes an array and returns a sorted array. It must use the bubble sort methodology
 
+numbers = [4,3,78,2,0,2]
+
 def bubble_sort(list)
-    list.each_with_index do |number, index|
-        
+    index = 0 
+    while index < list.size - 1 do 
+        if list[index] > list[index + 1]
+            list[index], list[index + 1] = list[index + 1], list[index]
+            index = 0
+        else index += 1  
+        end
     end 
+    list     
 end
 
-
-
-
-
-
+p bubble_sort(numbers)
