a1b23db0389ac24354eb3c8285e73dfac98a8419
Emil-ant <emiliosantoun411@gmail.com>
2023-02-13 00:21:01 +0000

    added initial function

--- Bubble_sort/bubble_sort.rb
+++ Bubble_sort/bubble_sort.rb
@@ -1,1 +1,13 @@
 # Build a method #bubble_sort that takes an array and returns a sorted array. It must use the bubble sort methodology
+
+def bubble_sort(list)
+    list.each_with_index do |number, index|
+        
+    end 
+end
+
+
+
+
+
+
